e157699accd3f33a9ca493edb4579f9ccd59aabb
swathireddyy <swathithillana12@gmail.com>
2020-06-27 12:25:31 +0530

    add.sh addran.sh arrayevennum.sh

--- add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-read -p "num x:" x
-read -p "num y:" y
-
-
-z=`expr $x + $y`
-echo $z
-
--- addran.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-a=$((RANDOM%6))
-b=$((RANDOM%6))
-sum=$(($a+$b))
-echo $a+$b=$sum
-
